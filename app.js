d57e39866a9bc6ca275a91aa902c665b1837dcff
MilleCodesLN <mille.helene@outlook.fr>
2023-02-01 11:40:23 +0100

    JS et fin de l'application

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Tableau des résultats à afficher suivant le résultat obtenu
 const BMIData = [
   { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
   { name: "Bonne santé", color: "green", range: [18.5, 25] },
@@ -8,3 +9,57 @@ const BMIData = [
 ];
 
 // IMC = poids en kg / taille² en m
+
+const form = document.querySelector("form");
+// On annule le comportement par défaut du bouton et on lance le calcul de l'IMC
+form.addEventListener("submit", handleForm);
+
+function handleForm(e) {
+  e.preventDefault();
+
+  calculateBMI();
+}
+
+const inputs = document.querySelectorAll("input");
+
+function calculateBMI() {
+  //  On récupère la valeur des inputs
+  const height = inputs[0].value;
+  const weight = inputs[1].value;
+  // console.log(height, weight);
+
+  // On valide les données reçues
+  if (!height || !weight || height <= 0 || weight <= 0) {
+    handleError();
+    return;
+  }
+
+
+  //   On calcule l'IMC
+  const BMI = (weight / Math.pow(height / 100, 2)).toFixed(1);
+
+  // On affiche le résultat
+  showResult(BMI);
+}
+
+const displayBMI = document.querySelector(".bmi-value");
+const result = document.querySelector(".result");
+
+function handleError() {
+    displayBMI.textContent = "Ooops!";
+    displayBMI.style.color = "inherit";
+    result.textContent = "Remplissez correctement les inputs.";
+
+
+}
+
+function showResult(BMI) {
+    const rank = BMIData.find(data => {
+        if(BMI>= data.range[0] && BMI < data.range[1]) return data;
+        else if(typeof data.range === "number" && BMI >= data.range) return data;
+    })
+
+    displayBMI.textContent = BMI;
+    displayBMI.style.color = `${rank.color}`;
+    result.textContent = `Résultat : ${rank.name}`;
+}
